f754a5cc4c5c7dd073a703ca3ada24d310b3eda1
choyon das <choyondas08@gmail.com>
2021-08-20 15:50:44 +0800

    phone price

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('I am in a separate file')
+document.getElementById('plusButton').addEventListener('click', function() {
+    const inputField = document.getElementById('inputField');
+    const previousValue = parseInt(inputField.value);
+    const newValue = previousValue + 1;
+    inputField.value = newValue;
+
+    const price = document.getElementById('price');
+    const previousPrice = parseInt(price.innerText);
+    const newPrice = previousPrice + 1219;
+    price.innerText = newPrice;
+
+});
+document.getElementById('minusButton').addEventListener('click', function() {
+    if (inputField.value >= 1) {
+        const inputField = document.getElementById('inputField');
+        const previousValue = parseInt(inputField.value);
+        const newValue = previousValue - 1;
+        inputField.value = newValue;
+
+
+        const price = document.getElementById('price');
+        const previousPrice = parseInt(price.innerText);
+        const newPrice = previousPrice - 1219;
+        price.innerText = newPrice;
+    }
+
+});
+const price = document.getElementById('price');
+const previousPrice = price.innerText;
